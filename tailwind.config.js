edb4ee0d07820b9d2c42fdf530228da1e60c0c01
DanielJH1135 <danieljhlee1135@gmail.com>
2026-05-30 23:41:39 +0900

    설정파일추가

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      animation: { marquee: 'marquee 25s linear infinite' },
+      keyframes: { marquee: { '0%': { transform: 'translateX(0%)' }, '100%': { transform: 'translateX(-100%)' } } }
+    }
+  },
+  plugins: []
+};
